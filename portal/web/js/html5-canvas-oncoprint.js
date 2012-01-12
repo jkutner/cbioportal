6b9e8d273ced5340a8f7bbdec5368480b4e2c9ae
Benjamin Gross <benjamin.gross@gmail.com>
2012-01-12 18:36:35 -0500

    Basic version of html5 canvas drawing.

--- portal/web/js/html5-canvas-oncoprint.js
+++ portal/web/js/html5-canvas-oncoprint.js
@@ -29,14 +29,173 @@
  * Javscript library based on HTML5 Canvas API which renders OncoPrints. 
  */
 
-// These are bits to pass as "AlterationSettings" in DrawAlteration
-var AMP     = (1<<0);
-var MUT     = (1<<1);
-var HOMDEL  = (1<<2);
-var HEMDEL  = (1<<3);
-var NORMAL  = (1<<4);
-var UPREG   = (1<<5);
-var DOWNREG = (1<<6);
+// These bits are passed as "AlterationSettings" in DrawAlteration -
+// they corresponded to names found in org.mskcc.portal.model.GeneticEventImpl
+// CNA bits
+var AMPLIFIED           = (1<<0);
+var GAINED              = (1<<1);
+var DIPLOID             = (1<<2);
+var HEMIZYGOUSLYDELETED = (1<<3);
+var HOMODELETED         = (1<<4);
+var NONE                = (1<<5);
+// MRNA bits (normal is in GeneticEventImpl, but never used)
+var UPREGULATED         = (1<<6);
+var DOWNREGULATED       = (1<<7);
+var NOTSHOWN            = (1<<8);
+// MUTATION bits
+var MUTATED             = (1<<9);
+var NORMAL              = (1<<10);
 
+// starting size of genetic alteration
+var ALTERATION_WIDTH = 6;
+var ALTERATION_HEIGHT = 18;
 
-the
+// space between genetic alterations
+var ALTERATION_VERTICAL_PADDING = 1;
+var ALTERATION_HORIZONTAL_PADDING = 1;
+
+// cna styles
+var AMPLIFIED_COLOR           = '#FF0000';
+var GAINED_COLOR              = '#FFB6C1';
+var DIPLOID_COLOR             = '#D3D3D3';
+var HEMIZYGOUSLYDELETED_COLOR = '#8FD8D8';
+var HOMODELETED_COLOR         = '#0000FF';
+var NONE_COLOR                = '#D3D3D3';
+
+// mrna styles
+var MRNA_RECT_WIREFRAME_THICKNESS = ALTERATION_WIDTH * (1/6);
+var UPREGULATED_COLOR   = '#FF9999';
+var DOWNREGULATED_COLOR = '#6699CC';
+var NOTSHOWN_COLOR      = '#FFFFFF';
+
+// mutation styles - if wd or ht changes, DrawMutation must change
+var MUTATION_RECT_WIDTH = ALTERATION_WIDTH - MRNA_RECT_WIREFRAME_THICKNESS;
+var MUTATION_RECT_HEIGHT = ALTERATION_HEIGHT * (1/3);
+var MUTATION_COLOR = '#008000'; // dark green
+
+/*
+ * Creates an HTML 5 canvas.
+ *
+ * parentID - DOM element where we append canvas
+ * canvasID - id to give to canvas
+ * numGenes - the number of rows
+ * numSamples - the number of columns
+ * scaleFactor - 1.0 no scale
+ */
+function CreateCanvas(parentID, canvasID, numGenes, numSamples, scaleFactor) {
+
+	var parentElement = document.getElementById(parentID);
+	if (parentElement) {
+		// we want enough space for each alteration w/padding.  remove padding on the right/bottom
+		var canvasHeight = numGenes * (ALTERATION_HEIGHT + ALTERATION_VERTICAL_PADDING) - ALTERATION_VERTICAL_PADDING;
+		var canvasWidth = numSamples * (ALTERATION_WIDTH + ALTERATION_HORIZONTAL_PADDING) - ALTERATION_HORIZONTAL_PADDING;
+ 		var canvas = document.createElement('canvas');
+		canvas.setAttribute('id', canvasID);
+		canvas.setAttribute('width', canvasWidth * scaleFactor);
+		canvas.setAttribute('height', canvasHeight * scaleFactor);
+		parentElement.appendChild(canvas);
+	}
+}
+
+/*
+ * Draws an alteration cell on the given canvas.
+ *
+ * canvasID - id of the canvas for drawing
+ * row - the row (gene) index
+ * column - the column (sample) index
+ * alterationSettings - the bits which describe what to draw
+ * scaling factor (1.0 - no scale)
+ */
+function DrawAlteration(canvasID, row, column, alterationSettings, scaleFactor) {
+
+	var canvasElement = document.getElementById(canvasID);
+	if (canvasElement && canvasElement.getContext) {
+		var context = canvasElement.getContext('2d');
+		if (context) {
+			context.save();
+			context.scale(scaleFactor, scaleFactor);
+			DrawMRNA(context, row, column, alterationSettings);
+			DrawCNA(context, row, column, alterationSettings);
+			DrawMutation(context, row, column, alterationSettings);			
+			context.restore();
+		}
+	}
+}
+
+/*
+ * Draws a CNA alteration cell on the given canvas.
+ */
+function DrawCNA(context, row, column, alterationSettings) {
+
+	if (alterationSettings & AMPLIFIED) {
+		context.fillStyle = AMPLIFIED_COLOR;
+	}
+	else if (alterationSettings & GAINED) {
+		context.fillStyle = GAINED_COLOR;
+	}
+	else if (alterationSettings & DIPLOID) {
+		context.fillStyle = DIPLOID_COLOR;
+	}
+	else if (alterationSettings & HEMIZYGOUSLYDELETED) {
+		context.fillStyle = HEMIZYGOUSLYDELETED_COLOR;
+	}
+	else if (alterationSettings & HOMODELETED) {
+		context.fillStyle = HOMODELETED_COLOR;
+	}
+	else if (alterationSettings & NONE) {
+		context.fillStyle = NONE_COLOR;
+	}
+	// we clear & then fill instead of
+	// using stroke because this is more precise
+	var rectWidth = ALTERATION_WIDTH-MRNA_RECT_WIREFRAME_THICKNESS*2;
+	var rectHeight = ALTERATION_HEIGHT-MRNA_RECT_WIREFRAME_THICKNESS*2;
+	var x = GetXCoordinate(column);
+	var y = GetYCoordinate(row);
+	// we want to draw CNA with a frame around it (MRNA_RECT_WIREFRAME_THICKNESS)
+	context.clearRect(x + MRNA_RECT_WIREFRAME_THICKNESS,
+					  y + MRNA_RECT_WIREFRAME_THICKNESS,
+					  rectWidth, rectHeight);
+	context.fillRect(x + MRNA_RECT_WIREFRAME_THICKNESS,
+					 y + MRNA_RECT_WIREFRAME_THICKNESS,
+					 rectWidth, rectHeight);
+}
+
+function DrawMRNA(context, row, column, alterationSettings) {
+
+	if (alterationSettings & UPREGULATED) {
+		context.fillStyle = UPREGULATED_COLOR;
+	}
+	else if (alterationSettings & DOWNREGULATED) {
+		context.fillStyle = DOWNREGULATED_COLOR;
+	}
+	else if (alterationSettings & NOTSHOWN) {
+		context.fillStyle = NOTSHOWN_COLOR;
+	}
+	context.lineWidth = MRNA_RECT_WIREFRAME_THICKNESS;
+	// x, y, width, height
+	var x = GetXCoordinate(column);
+	var y = GetYCoordinate(row);
+	context.fillRect(x, y, ALTERATION_WIDTH, ALTERATION_HEIGHT);
+}
+
+function DrawMutation(context, row, column, alterationSettings) {
+
+	if (alterationSettings & MUTATED) {
+		context.fillStyle = MUTATION_COLOR;
+		// translate row/col into canvas coordinates
+		var x = GetXCoordinate(column);
+		var y = GetYCoordinate(row);
+		// center mutation square vertical & start drawing halfway into MRNA WIREFRAME
+		context.fillRect(x + MRNA_RECT_WIREFRAME_THICKNESS / 2,
+						 y + ALTERATION_HEIGHT / 2 - MUTATION_RECT_HEIGHT / 2,
+						 MUTATION_RECT_WIDTH, MUTATION_RECT_HEIGHT);
+	}
+}
+
+function GetXCoordinate(column) {
+	return column * (ALTERATION_WIDTH + ALTERATION_HORIZONTAL_PADDING);
+}
+
+function GetYCoordinate(row) {
+	return row * (ALTERATION_HEIGHT + ALTERATION_VERTICAL_PADDING);
+}
